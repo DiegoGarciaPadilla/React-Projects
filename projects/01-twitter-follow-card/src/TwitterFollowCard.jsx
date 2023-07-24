5c5990c05f1ea04607b9779c9f9d3c2085c9f988
Diego Garcia Padilla <gpdiego370@outlook.com>
2023-07-24 13:56:33 -0600

    Update TwitterFollowCard.jsx
    Update button

--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -28,7 +28,8 @@ export function TwitterFollowCard({
       </header>
       <aside className="tw-followCard-aside">
         <button className={buttonClassName} onClick={handleClick}>
-          {text}
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
         </button>
       </aside>
     </article>
